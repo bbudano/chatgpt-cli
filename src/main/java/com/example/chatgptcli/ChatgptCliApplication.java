53ccd5aec528f1998d370456769803789f5abd5a
Bernard Budano <bbudano@gmail.com>
2023-02-10 17:00:50 +0100

    Initial commit

---
+++ src/main/java/com/example/chatgptcli/ChatgptCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatgptcli/ChatgptCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
