35d3442d05d798da94402dde9217d16525e8641f
Bernard Budano <bbudano@gmail.com>
2023-05-12 23:55:08 +0200

    Renamed shell methods to match chat gpt API

--- src/main/java/com/example/chatgptcli/shell/ChatGPTCommands.java
+++ src/main/java/com/example/chatgptcli/shell/ChatGPTCommands.java
@@ -13,18 +13,18 @@ public class ChatGPTCommands {
     private final ChatGPTHttpService chatgptHttpService;
 
     @ShellMethod("Make request to ChatGPT Completions API")
-    public String ask(String prompt,
-                      @ShellOption(defaultValue = "text-davinci-003") String model,
-                      @ShellOption(defaultValue = "0.2") Double temperature,
-                      @ShellOption(value = "max-tokens", defaultValue = "100") Integer maxTokens) {
+    public String completions(String prompt,
+                              @ShellOption(defaultValue = "text-davinci-003") String model,
+                              @ShellOption(defaultValue = "0.2") Double temperature,
+                              @ShellOption(value = "max-tokens", defaultValue = "100") Integer maxTokens) {
         return chatgptHttpService.requestCompletion(prompt, model, temperature, maxTokens);
     }
 
     @ShellMethod("Make request to ChatGPT Images API")
-    public String img(String prompt,
-                      @ShellOption(defaultValue = "1") Integer n,
-                      @ShellOption(defaultValue = "1024x1024") String size,
-                      @ShellOption(value = "response_format", defaultValue = "url") String responseFormat) {
+    public String images(String prompt,
+                         @ShellOption(defaultValue = "1") Integer n,
+                         @ShellOption(defaultValue = "1024x1024") String size,
+                         @ShellOption(value = "response_format", defaultValue = "url") String responseFormat) {
         return chatgptHttpService.requestImage(prompt, n, size, responseFormat);
     }
 
